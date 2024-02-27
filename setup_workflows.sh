a9172bb75a93a4fcc7c9141eb71eb2007fdbe621
Mustafa <mustafa.cetin@intel.com>
2024-02-27 19:52:02 +0000

    update fsspec version

--- setup_workflows.sh
+++ setup_workflows.sh
@@ -7,3 +7,4 @@ pip install -r ${PWD}/vision_wf/workflows/disease_prediction/requirements.txt
 pip install -r ${PWD}/hf_nlp/workflows/hf_finetuning_and_inference_nlp/requirements.txt
 
 pip install accelerate "transformers<4.30.0"
+pip install fsspec==2023.9.2
